5bc0824334d6fed65527dd2998b8a7a0edd14cf1
Adam Spencer <aspencer0919@gmail.com>
2020-05-01 21:52:56 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,17 @@
+class Doctor
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(patient)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
