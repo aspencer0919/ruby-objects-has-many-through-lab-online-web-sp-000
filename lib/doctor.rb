664ceb1a47dcddbd7556b5946e2faf6d6775ba2e
Adam Spencer <aspencer0919@gmail.com>
2020-05-01 21:37:18 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(date, doctor, patient)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+end
